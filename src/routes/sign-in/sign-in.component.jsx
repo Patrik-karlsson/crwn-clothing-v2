bb953dec06f490bed54f6cb4318f153c52b87301
Patrik <patrik.karlsson23@gmail.com>
2022-09-17 16:02:48 +0200

    added sign in component added firebase and ability to create users

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -5,16 +5,20 @@ import {
 } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
+  //  whenever you make a call to some database, this is going to be asynchronous.
   const logGoogleUser = async () => {
-    //
+    // So we want to get the value here.By awaiting calling or signing with Google pop up.
     const { user } = await signInWithGooglePopup();
-    // createUserdocumentFromAut gets called via the user object in the response document
+    // createUserdocumentFromAut gets called via the user object in the response document from the signIn
     createUserdocumentFromAuth(user);
+
+    const userDocRef = await createUserdocumentFromAuth(user);
   };
 
   return (
     <div>
       <h1>Sign In Page</h1>
+      {/* we're going to set the on click method so that it uses our log Google user method. */}
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
     </div>
   );
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -7,6 +7,10 @@ import {
 } from 'firebase/auth';
 
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+// getFirestore: allows developers to make CRUD actions
+// doc:lets us retrieve documents from firestore database
+// getDoc: lets us retrive data from the documents
+// setDoc: lets us set data on documents
 const firebaseConfig = {
   apiKey: 'AIzaSyDALL2Lp-JGGNuiTFYY02Uhw7z1j4ADZmc',
   authDomain: 'crwn-f605f.firebaseapp.com',
@@ -15,33 +19,40 @@ const firebaseConfig = {
   messagingSenderId: '630062953188',
   appId: '1:630062953188:web:ceb8e59afbc19b5517f2f1',
 };
-
+// creates an app instance/object based on the config by identifying the relevant Software Developer Kit pr SDK
 const firebaseApp = initializeApp(firebaseConfig);
 
+// Google auth provider, which in turn will give you back this provider instance.
+
+// These providers are kind of just instructions for this instance of provider, but you can have multiple different providers authentication.
 const provider = new GoogleAuthProvider();
 
+// custom parameters will take some kind of configuration object and with it we can tell different ways that we want this Google auth provider to behave.
 provider.setCustomParameters({
+  // prompt means is that every time somebody interacts with our provider, we want to always force them to select an account.
   prompt: 'select_account',
 });
 
-// instantiate objects section
+// instantiate (or create) objects section: by exporting the the instance
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
   console.log(userAuth);
 };
-
+// even with several providers that all have their own authentication. The application in itself only ever needs one authentivation
 export const auth = getAuth();
+
+// exporting out sign in with popup
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
-// singelton instance that directly point to the DATABASE that tells firestore when to get or set a document IE data
+
+//this Singleton instance allows us now to tell Firebase when we want to get a document or we want to set a document or anything like that related to our database.
 export const database = getFirestore();
 
-// In order to use the database this "method" or async function receives data from the authentication service. like Google. And then stores that inside of FIRESTORE.
+// In order to use the database this "method" or async function receives data from the authentication service. like from the Google authProvider. And then stores that inside of FIRESTORE.
 export const createUserdocumentFromAuth = async (userAuth) => {
-  //
   // userDocRef checks if there is an existing document reference and takes in three arguments:
   // database:stores the database instance
-  // collections: users
+  // collections: (in this case called) users.
   // identifier: a unique ID or UID that contains groupings of data
   const userDocRef = doc(database, 'users', userAuth.uid);
   // logs the userDocRef instance
@@ -52,6 +63,38 @@ export const createUserdocumentFromAuth = async (userAuth) => {
   // referenses userSnapshot
   console.log(userSnapshot);
   // userSnapshot allows for leveraging the .exists method which checks if that document exists in the firebase Database as collections.
-  // that verification is for checking if there is a ducument storing the data already. If so we can ask for it If not we can create a document for that data.
+  // that verification is for checking if there is a document storing the data already in the database. If so we can ask for it If not we can create a document for that data.
   console.log(userSnapshot.exists());
+
+  // How to create/ set the document with the data from user in the collection?
+  // because the user snapshot is already pointing to a specific place in a collection for the data we want. ¨
+
+  //  the bang operator, which means that if the snapshot. exists then it'll return true.If it doesn't exist, it gives us false.
+  // So that means that if it doesn't exist, this will return true.
+  if (!userSnapshot.exists()) {
+    // if the document does not exist. To then create and set the document. One needs to extract the displayName and email from the user auth object (the fields on the object alongside the UID)
+    // which can be retrived from the userAuth object
+    const { displayName, email } = userAuth;
+    // to get the snapshot to display the Date the users are signing in
+    const createdAt = new Date();
+    // Then in order to set this document, we need a try catch block.
+    // so if setting the document does not work the catch is going to display the error.
+    try {
+      // if get doc gets us the document, then set doc allows us to set the document.
+      // And similarly, what we're going to pass it is a user doc reference that we just got back using the doc and then we're going to pass it the data we want to set it with.
+      //  we're going to pass it the data we want to set it with from the doc ("or userDocRef")
+      // So here we want to set a display name, we want to set a email and we want to set a created at date.
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      // logging custom error message as well as the error.message
+      console.log('error creting the user', error.message);
+    }
+  }
+  // So we have now created a way to create users.
+  //   using this system we now have authentication as well as storage of these users inside of our application.
+  return userDocRef;
 };
